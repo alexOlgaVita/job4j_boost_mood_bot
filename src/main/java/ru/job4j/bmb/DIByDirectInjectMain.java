af11ce20a95a8ad053770308e6f0976371f3b4cc
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-27 20:05:40 +0300

    @Service и @Repository | BotCommandHandler - обработка комманд [#505306]

--- src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
+++ src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
@@ -11,8 +11,10 @@ import ru.job4j.bmb.services.TelegramBotService;
  */
 public class DIByDirectInjectMain {
     public static void main(String[] args) {
+/*
         var handler = new BotCommandHandler();
         var tg = new TelegramBotService(handler);
         tg.content(new Content(1L));
+ */
     }
 }
--- src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
+++ src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
@@ -1,8 +1,15 @@
 package ru.job4j.bmb.services;
 
-import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.model.User;
+import ru.job4j.bmb.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * Класс будет обрабатывать меню бота.
@@ -10,13 +17,58 @@ import ru.job4j.bmb.content.Content;
  * @version 1.0
  */
 @Service
-public class BotCommandHandler implements BeanNameAware {
-    void receive(Content content) {
-        System.out.println(content);
+public class BotCommandHandler {
+    private final UserRepository userRepository;
+    private final MoodService moodService;
+    private final TgUI tgUI;
+
+    public BotCommandHandler(UserRepository userRepository,
+                             MoodService moodService,
+                             TgUI tgUI) {
+        this.userRepository = userRepository;
+        this.moodService = moodService;
+        this.tgUI = tgUI;
     }
 
-    @Override
-    public void setBeanName(String beanName) {
-        System.out.println(beanName);
+    /**
+     * Обработка команд
+     * @param message
+     * @return
+     */
+    Optional<Content> commands(Message message) {
+        switch (message.getText()) {
+            case  ("/start"):
+                return handleStartCommand(message.getChatId(), message.getContact().getUserId());
+                break;
+            case ("/week_mood_log"):
+                return moodService.weekMoodLogCommand(message.getChatId(), message.getContact().getUserId());
+                break;
+            case ("/month_mood_log"):
+                return moodService.monthMoodLogCommand(message.getChatId(), message.getContact().getUserId());
+                break;
+            case ("/award"):
+                return moodService.awards(message.getChatId(), message.getContact().getUserId());
+                break;
+            default:
+                return Optional.empty();
+                break;
+        }
+    }
+
+    Optional<Content> handleCallback(CallbackQuery callback) {
+        var moodId = Long.valueOf(callback.getData());
+        var user = userRepository.findByClientId(callback.getFrom().getId());
+        return Stream.of(user).map(value -> moodService.chooseMood(value, moodId)).findFirst();
+    }
+
+    private Optional<Content> handleStartCommand(long chatId, Long clientId) {
+        var user = new User();
+        user.setClientId(clientId);
+        user.setChatId(chatId);
+        userRepository.save(user);
+        var content = new Content(user.getChatId());
+        content.setText("Как настроение?");
+        content.setMarkup(tgUI.buildButtons());
+        return Optional.of(content);
     }
 }
