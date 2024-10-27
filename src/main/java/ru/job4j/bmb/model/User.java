0c7f2047e05e4ddd929a557e0e8bb33a81ec80b8
olga.ilyina <olga.ilyina@axenix.pro>
2024-10-27 13:19:37 +0300

    Spring Data JPA [#505298]

--- src/main/java/ru/job4j/bmb/model/User.java
+++ src/main/java/ru/job4j/bmb/model/User.java
@@ -1,15 +1,20 @@
 package ru.job4j.bmb.model;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Repository;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
-@Profile("test")
-@Repository
+@Entity
+@Table(name = "mb_user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "client_id")
     private long clientId;
+
+    @Column(name = "chat_id")
     private long chatId;
 
     public User() {
@@ -55,13 +60,11 @@ public class User {
             return false;
         }
         User user = (User) o;
-        return id == user.id
-                && clientId == user.clientId
-                && chatId == user.chatId;
+        return Objects.equals(id, user.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, clientId, chatId);
+        return Objects.hash(id);
     }
 }
